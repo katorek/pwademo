c3fe19320200209b773f68211df58280a64b6bc9
Wojciech Jaroński <wojtasj1996@gmail.com>
2019-04-29 18:37:47 +0200

    Add missing module

---
+++ src/app/meal-detail/meal-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './meal-detail.component.html',
+  styleUrls: ['./meal-detail.component.scss']
+})
+export class MealDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
